76b781ff9a5ed70354371b802acddc822cfaffdd
xiangyu <xiangyukongdeveloper@gmail.com>
2021-04-11 20:17:12 -0400

    fix textContent bug

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -50,10 +50,6 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  console.log('columnEl:', columnEl);
-  console.log('column:', column);
-  console.log('item:', item);
-  console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
@@ -82,11 +78,11 @@ function updateDOM() {
   });
 
   // Complete Column
-  
   completeList.textContent = '';
   completeListArray.forEach((completeItem, index) => {
     createItemEl(completeList, 0, completeItem, index);
   });
+
   // On Hold Column
   onHoldList.textContent = '';
   onHoldListArray.forEach((onHoldItem, index) => {
@@ -102,19 +98,19 @@ function updateDOM() {
 function rebuildArrays() {
   backlogListArray = [];
   for (let i = 0; i < backlogList.children.length; i++) {
-    backlogListArray.push(backlogList.children[i]);
+    backlogListArray.push(backlogList.children[i].textContent);
   }
   progressListArray = [];
   for (let i = 0; i < progressList.children.length; i++) {
-    progressListArray.push(progressList.children[i]);
+    progressListArray.push(progressList.children[i].textContent);
   }
   completeListArray = [];
   for (let i = 0; i < completeList.children.length; i++) {
-    completeListArray.push(completeList.children[i]);
+    completeListArray.push(completeList.children[i].textContent);
   }
   onHoldListArray = []
   for (let i = 0; i < onHoldList.children.length; i++) {
-    onHoldListArray.push(onHoldList.children[i]);
+    onHoldListArray.push(onHoldList.children[i].textContent);
   }
   updateDOM();
 }

f5814aebb0e8cbcbc6456bdc53799819901df3eb
xiangyu <xiangyukongdeveloper@gmail.com>
2021-04-24 13:47:18 -0400

    add progress bar and volume control

--- video-player/script.js
+++ video-player/script.js
@@ -1,16 +1,111 @@
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn= document.querySelector('.fullscreen');
 
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+}
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
+
+// On Video End
+video.addEventListener('ended', showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+  return `${minutes}:${seconds}`;
+}
+
+// Update preogress bar as video plays
+function updateProgress() {
+  // 这里修改的是CSS property 所以需要加上% 来表示百分比
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`
+}
+
+// Click to seek within the video
+function setProgress(e) {
+  console.log(e);
+  // 新的时间点的百分比
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+  console.log(newTime);
+}
 
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
 
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  volumeIcon.className = '';
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down');
+  } else if (volume === 0 ) {
+    volumeIcon.classList.add('fas', 'fa-volume-off');
+  }
+
+  lastVolume = volume;
+}
+
+// Mute/Unmute
+function toggleMute() {
+volumeIcon.className = '';
+
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add('fas', 'fa-volume-mute');
+    volumeIcon.setAttribute('title', 'Unmute');
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    if (lastVolume > 0.7) { 
+      volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if (lastVolume < 0.7 && lastVolume > 0) {
+      volumeIcon.classList.add('fas', 'fa-volume-down');
+    }
+    volumeIcon.setAttribute('title', 'Mute');
+  }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -19,3 +114,11 @@
 // Fullscreen ------------------------------- //
 
 
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
